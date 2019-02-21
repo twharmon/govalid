3c15c431d856afea6ac1405b8d852c7dd99249db
Travis Harmon <trvshrmn@gmail.com>
2019-02-21 15:47:05 -0500

    AddCustom to take variadic arg

--- constraint.go
+++ constraint.go
@@ -12,15 +12,15 @@ type constraint interface {
 //
 // NOTE: This is not thread safe. You must
 // add cusrom validation functions before validating.
-func AddCustom(s interface{}, f func(interface{}) ([]string, error)) {
+func AddCustom(s interface{}, f ...func(interface{}) ([]string, error)) {
 	t := reflect.TypeOf(s)
 	if t.Kind() == reflect.Ptr {
-		panic("pointers can not be registered")
+		panic("s can not be a pointer")
 	}
 	n := t.Name()
 	m := modelStore[n]
 	if m == nil {
 		panic("struct s must be registered before adding a custom validator")
 	}
-	m.custom = append(m.custom, f)
+	m.custom = append(m.custom, f...)
 }
